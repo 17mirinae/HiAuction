594137fe3155ea69e0dd1d007ea789c343d3f302
17mirinae <mirinae@skuniv.ac.kr>
2019-11-24 19:50:08 +0900

    HiAuction Update

--- app/src/main/java/com/example/mobileapp/SignUpPage.java
+++ app/src/main/java/com/example/mobileapp/SignUpPage.java
@@ -5,22 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignUpPage extends AppCompatActivity {
     final static private String URL = "http://cslin.skuniv.ac.kr/~chungmj1767/Mobile/SignUpPHP.php";
 
     Button signUpBtn;
     EditText userIdEdit, userPwdEdit, userAddrEdit, userPhoneEdit;
+    String userId, userPwd, userAddr, userPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,59 +40,38 @@ public class SignUpPage extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SignUp();
+                userId = userIdEdit.getText().toString();
+                userPwd = userPwdEdit.getText().toString();
+                userAddr = userAddrEdit.getText().toString();
+                userPhone = userPhoneEdit.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "Sign Up Clear", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpPage.this);
+                                builder.setMessage("Try Again")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }; // Response.Listener<String> End
+
+                RegisterRequest registerRequest = new RegisterRequest(userId, userPwd, userAddr, userPhone, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(SignUpPage.this);
+                queue.add(registerRequest);
             } // onClick End
         }); // signUpBtn.setOnClickListener End
     }
-
-    private void SignUp() {
-        final String userId = this.userIdEdit.getText().toString().trim();
-        final String userPwd = this.userPwdEdit.getText().toString().trim();
-        final String userAddr = this.userAddrEdit.getText().toString().trim();
-        final String userPhone = this.userPhoneEdit.getText().toString().trim();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
-            }
-        };
-    }
 }
-//    Response.Listener<String> responseListener = new Response.Listener<String>() {
-//        @Override
-//        public void onResponse(String response) {
-//            try {
-//                JSONObject jsonResponse = new JSONObject(response);
-//                boolean success = jsonResponse.getBoolean("success");
-//
-//                if (success) {
-//                    Toast.makeText(getApplicationContext(), "SignUp Success", Toast.LENGTH_SHORT);
-//                    finish();
-//                } else {
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpPage.this);
-//                    builder.setMessage("Check Your ID")
-//                            .setNegativeButton("Retry", null)
-//                            .create()
-//                            .show();
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }; // Response.Listener<String> End
-//
-//    RequestQueue queue = Volley.newRequestQueue(SignUpPage.this);
-//        queue.add(registerRequest);
